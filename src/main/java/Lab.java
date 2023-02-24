bf79f115df4c714892fa40d7ada28e95e0489fde
sushmishra87 <sm.sushmimishra@gmail.com>
2023-02-24 00:59:46 +0000

    Completed Lab EvenOrOddTest

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,5 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class Lab {
     /**
      * Your challenge is to implement a method to determine if an int is even or odd. The method should return
@@ -13,7 +15,13 @@ public class Lab {
      * @param n a number.
      * @return a String "Even" if n is even, and "Odd" of in is odd. The test cases are case-sensitive.
      */
-    public String decide(int n){
-        return "";
+    public String decide(int n)
+    {
+        if(n%2 == 0)
+        {
+            return "Even";
+        }
+
+        return "Odd";
     }
 }
